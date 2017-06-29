4c8f04f20dd484183522dadbaadb98ce5bc4feab
EchoXBR <372183365@qq.com>
2017-06-29 17:58:40 +0800

    添加图像采集

--- app/src/main/java/com/speedata/yilang/RegisterActivity.java
+++ app/src/main/java/com/speedata/yilang/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.speedata.yilang;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.content.ContentUris;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -13,6 +15,7 @@ import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,6 +45,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle(R.string.btn_zhuce);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         edvName = (EditText) findViewById(R.id.edv_name);
@@ -108,6 +112,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         if (v == btnRegister) {
             //TODO 用户信息写入卡中
+            if (write_photo == null) {
+                Toast.makeText(this, "please get photo first!", Toast.LENGTH_SHORT).show();
+                return;
+            }
             ProgressDialogUtils.showProgressDialog(RegisterActivity.this, getString(R.string.updata));
 
             new Thread(new Runnable() {
@@ -129,10 +137,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                     final boolean result1 = cardManager.updateUserInfor(JSEscape.getNameByte(name + "," + Sex));
                     //TODO 更新性别
 
-                    Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.mipmap.photo_1);
 
-                    final boolean result2 = cardManager.updatePhotoInfor(
-                            ImgCompress.compressBitmap(ImgCompress.decodeSampledBitmapFromResource(getResources(), resPhotoID, 100, 100)));
+                    final boolean result2 = cardManager.updatePhotoInfor(write_photo);
+//                            ImgCompress.compressBitmap(
+//                                    ImgCompress.decodeSampledBitmapFromResource(getResources(), resPhotoID, 100, 100)));
 
                     runOnUiThread(new Runnable() {
                         @Override
@@ -151,137 +159,181 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
 
         } else if (v == imageButtonGetPhoto) {
-            // 获取图像并显示
-            File imageFile = new File(Environment
-                    .getExternalStorageDirectory(), "outputImage.jpg");
-            if (imageFile.exists()) {
-                imageFile.delete();
-            }
-            try {
-                imageFile.createNewFile();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-            }
-            //转换成Uri
-            imageUri = Uri.fromFile(imageFile);
-            //开启选择呢绒界面
-            Intent intent = new Intent("android.intent.action.GET_CONTENT");
-            //设置可以缩放
-            intent.putExtra("scale", true);
-            //设置可以裁剪
-            intent.putExtra("crop", true);
-            intent.setType("image/*");
-            //设置输出位置
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-            //开始选择
-            startActivityForResult(intent, CHOOSE_PHOTO);
+            showDialog();
         }
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            // 从拍照界面返回
-            case TAKE_PHOTO:
-                if (resultCode == RESULT_OK) {
-                    // 设置intent为启动裁剪程序
-                    Intent intent = new Intent("com.android.camera.action.CROP");
-                    // 设置Data为刚才的imageUri和Type为图片类型
-                    intent.setDataAndType(imageUri, "image/*");
-                    // 设置可缩放
-                    intent.putExtra("scale", true);
-                    // 设置输出地址为imageUri
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                    // 开启裁剪,设置requestCode为CROP_PHOTO
-                    startActivityForResult(intent, CROP_PHOTO);
-                }
-
-                break;
-            // 从裁剪界面返回
-            case CROP_PHOTO:
-                if (resultCode == RESULT_OK) {
-                    Bitmap bitmap;
-                    try {
-                        //通过BitmapFactory将imageUri转化成bitmap
-                        bitmap = BitmapFactory.decodeStream(getContentResolver()
-                                .openInputStream(imageUri));
-                        //设置显示
-                        imgPhoto.setImageBitmap(bitmap);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                break;
-            case CHOOSE_PHOTO:
-                if (resultCode == RESULT_OK) {
-                    handleImageOnKitkat(data);
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    private void handleImageOnKitkat(Intent data) {
-        String imagePath = null;
-        Uri uri = data.getData();
-        if (DocumentsContract.isDocumentUri(this, uri)) {
-            // 如果是document类型的Uri，则通过document id处理
-            String docId = DocumentsContract.getDocumentId(uri);
-            if ("com.android.providers.media.documents".equals(uri
-                    .getAuthority())) {
-                String id = docId.split(":")[1]; // 解析出数字格式的id
-                String selection = MediaStore.Images.Media._ID + "=" + id;
-                imagePath = getImagePath(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
-            } else if ("com.android.providers.downloads.documents".equals(uri
-                    .getAuthority())) {
-                Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"),
-                        Long.valueOf(docId));
-                imagePath = getImagePath(contentUri, null);
-            }
-        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
-            // 如果不是document类型的Uri，则使用普通方式处理
-            imagePath = getImagePath(uri, null);
-        }
-        displayImage(imagePath); // 根据图片路径显示图片
-        System.err.println(imagePath);
-    }
-
-    private String getImagePath(Uri uri, String selection) {
-        String path = null;
-        // 通过uri和selection来获取真实的图片路径
-        Cursor cursor = getContentResolver().query(uri, null, selection, null,
-                null);
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                path = cursor.getString(cursor
-                        .getColumnIndex(MediaStore.Images.Media.DATA));
-            }
-            cursor.close();
-        }
-        return path;
-    }
 
     Bitmap bitmap;
 
-    private void displayImage(String imagePath) {
-        if (imagePath != null) {
-            bitmap = BitmapFactory.decodeFile(imagePath);
-            imgPhoto.setImageBitmap(bitmap);
-        } else {
-            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT)
-                    .show();
-        }
-    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 //        cardManager.realsePsam();
     }
+
+
+    // 拍照存放本地文件名称
+    public static final String IMAGE_FILE_NAME = "faceImage.jpg";
+    // 裁剪后的文件名称
+    public static final String IMAGE_FILE_NAME_TEMP = "faceImage_temp.jpg";
+
+    private File file = new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME);
+
+    private File cropFile = new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME_TEMP);
+    private Uri imageCropUri = Uri.fromFile(cropFile);
+    // 上传头像方式文字数据
+    private String[] items = new String[]{"选择本地图片", "拍照"};
+
+    // 请求码
+    private static final int IMAGE_REQUEST_CODE = 0;
+    private static final int CAMERA_REQUEST_CODE = 2;
+    private static final int RESULT_REQUEST_CODE = 3;
+
+
+    /**
+     * 选择是拍照还是选择本地图片
+     */
+    private void showDialog() {
+
+        new AlertDialog
+                .Builder(this).setTitle("设置头像").setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        //从相册选择
+                        Intent intent = new Intent(Intent.ACTION_PICK, null);
+                        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                        startActivityForResult(intent, IMAGE_REQUEST_CODE);
+                        break;
+                    case 1:
+                        //拍照
+                        Intent intentFromCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        // 判断存储卡是否可以用，可用进行存储
+                        if (hasSdcard()) {
+                            intentFromCapture.putExtra("return-data", false);
+                            imageUri = Uri.fromFile(file);
+                            intentFromCapture.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                            intentFromCapture.putExtra("noFaceDetection", true);
+                        }
+                        startActivityForResult(intentFromCapture, CAMERA_REQUEST_CODE);
+                        break;
+                }
+            }
+        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).show();
+
+    }
+
+    /**
+     * 判断sdcard是否存在
+     */
+    private boolean hasSdcard() {
+        // 判断Sdcard是否可用
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else
+            return false;
+    }
+
+    /**
+     * 处理回转值
+     */
+    @SuppressWarnings("static-access")
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != this.RESULT_CANCELED) {
+            switch (requestCode) {
+                case IMAGE_REQUEST_CODE:
+                    // 裁剪图片方法
+                    startPhotoZoom(data.getData());
+                    break;
+                // 保存头像
+                case CAMERA_REQUEST_CODE:
+                    if (hasSdcard()) {
+                        startPhotoZoom(imageUri);
+                    } else {
+                        Toast.makeText(this, "未找到存储卡，无法存储照片！", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                // 裁剪之后，显示头像
+                case RESULT_REQUEST_CODE:
+                    if (data != null) {
+                        setImageToView(data);
+                    }
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 裁剪图片方法实现
+     */
+    public void startPhotoZoom(Uri uri) {
+        if (uri == null) {
+            Log.i("tag", "========The uri is not exist.");
+        }
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("scale", true);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        //裁剪之后，保存在裁剪文件中，关键
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageCropUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+        intent.putExtra("return-data", false);
+        startActivityForResult(intent, RESULT_REQUEST_CODE);
+    }
+
+    private byte[] write_photo;
+
+    /**
+     * 保存裁剪之后的图片数据,并显示
+     */
+    private void setImageToView(Intent data) {
+        Bundle extras = data.getExtras();
+        Bitmap bitmap = null;
+
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageCropUri));
+            Log.d("size", bitmap.getByteCount() + "");
+            write_photo = ImgCompress.getPicByte(bitmap);
+            System.out.println("photo==" + write_photo.length);
+            imgPhoto.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (extras != null) {
+            // bitmap = extras.getParcelable("data");
+            // Log.d("size",bitmap.getByteCount()+"");
+
+            /*
+             * ByteArrayOutputStream stream = new ByteArrayOutputStream();
+             * bitmap.compress(Bitmap.CompressFormat.JPEG, 60, stream);
+             * byte[] b = stream.toByteArray(); // 将图片流以字符串形式存储下来
+             *
+             * tp = new String(Base64Coder.encodeLines(b));
+             * 这个地方大家可以写下给服务器上传图片的实现，直接把tp直接上传就可以了， 服务器处理的方法是服务器那边的事了，吼吼
+             *
+             * 如果下载到的服务器的数据还是以Base64Coder的形式的话，可以用以下方式转换 为我们可以用的图片类型就OK啦...吼吼
+             * Bitmap dBitmap = BitmapFactory.decodeFile(tp);
+             * Drawable drawable = new BitmapDrawable(dBitmap);
+             */
+        }
+    }
+
 }
